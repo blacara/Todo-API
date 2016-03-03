8696e250d0fbf56071354632840222726af96b01
Benjamin LaCara <ben.lacara@gmail.com>
2016-03-03 11:15:03 -0800

    add login route and authenticare class method

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
-var bcrypt = require('bcryptjs');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -32,9 +32,11 @@ app.get('/todos', function(req, res) {
 		};
 	}
 
-	db.todo.findAll({where: where}).then(function (todos) {
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
 		res.json(todos);
-	}, function (e) {
+	}, function(e) {
 		res.status(500).send();
 	});
 });
@@ -43,13 +45,13 @@ app.get('/todos', function(req, res) {
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoId).then(function (todo) {
+	db.todo.findById(todoId).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
 			res.status(404).send();
 		}
-	}, function (e) {
+	}, function(e) {
 		res.status(500).send();
 	});
 });
@@ -58,9 +60,9 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	db.todo.create(body).then(function (todo) {
+	db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
-	}, function (e) {
+	}, function(e) {
 		res.status(400).json(e);
 	});
 });
@@ -68,22 +70,22 @@ app.post('/todos', function(req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-    
-    db.todo.destroy({
-        where: {
-            id: todoId
-        }
-    }).then(function(rowsDeleted) {
-        if (rowsDeleted === 0) {
-            res.status(404).json({
-                error: 'No todo with id'
-            });
-        } else {
-            res.status(204).send();
-        }
-    }, function () {
-        res.status(500).send();
-    });
+
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted) {
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function() {
+		res.status(500).send();
+	});
 });
 
 // PUT /todos/:id
@@ -92,32 +94,31 @@ app.put('/todos/:id', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 	var attributes = {};
 
-	if (body.hasOwnProperty('completed')){
+	if (body.hasOwnProperty('completed')) {
 		attributes.completed = body.completed;
 	}
-    
+
 	if (body.hasOwnProperty('description')) {
 		attributes.description = body.description;
 	}
 
 	db.todo.findById(todoId).then(function(todo) {
-        if (todo) {
-            todo.update(attributes).then(function(todo) {
-                res.json(todo.toJSON());
-            }, function (e) {
-                res.status(400).json(e);
-    })
-        } else {
-            res.status(404).send();
-        }
-    }, function () {
-        res.status(500).send();
-    });
+		if (todo) {
+			todo.update(attributes).then(function(todo) {
+				res.json(todo.toJSON());
+			}, function(e) {
+				res.status(400).json(e);
+			});
+		} else {
+			res.status(404).send();
+		}
+	}, function() {
+		res.status(500).send();
+	});
 });
 
-
 app.post('/users', function (req, res) {
-    var body = _.pick(req.body, 'email', 'password');
+	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function (user) {
 		res.json(user.toPublicJSON());
@@ -126,17 +127,16 @@ app.post('/users', function (req, res) {
 	});
 });
 
+// POST /users/login
+app.post('/users/login', function (req, res) {
+	var body = _.pick(req.body, 'email', 'password');
 
-app.post('/users/login', function(req, res) {
-    var body = _.pick(req.body, 'email', 'password');
-    
-    db.user.authenticate(body).then(function(user) {
-        res.json(user.toPublicJSON());
-    }, function() {
-        res.status(401).send();
-    });
+	db.user.authenticate(body).then(function (user) {
+		res.json(user.toPublicJSON());
+	}, function () {
+		res.status(401).send();
+	});
 });
-
 
 db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
