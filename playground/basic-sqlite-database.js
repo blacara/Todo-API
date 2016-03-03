b5c8fb0ffced2ace39727965db1392d60414ac6a
Benjamin LaCara <ben.lacara@gmail.com>
2016-03-03 14:46:03 -0800

    added todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -17,42 +17,42 @@ var Todo = sequelize.define('todo', {
         allowNull: false,
         defaultValue: false
     }
-})
+});
 
-sequelize.sync({}).then(function () {
+var User = sequelize.define('user', {
+    email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
+sequelize.sync({
+    // force: true
+}).then(function () {
     console.log("Everything is synced");
     
-    Todo.findById(2).then(function(todo) {
-        if (todo) {
-            console.log(todo.toJSON());
-        } else {
-            console.log("todo not found");
-        }
+    User.findById(1).then(function(user) {
+        user.getTodos({
+            where: {
+                completed: false
+            }
+        }).then(function(todos) {
+            todos.forEach(function(todo) {
+                console.log(todo.toJSON());
+            })
+        });
     });
     
-    // Todo.create({
-    //     description: 'Take out trash'
-    // }).then (function(todo) {
-    //     return Todo.create({
-    //         description: 'Clean office'
-    //     });
+    // User.create({
+    //     email: "test@email.com"
     // }).then(function() {
-    //     return Todo.findAll({
-    //         where: {
-    //             description: {
-    //                 $like: '%trash%'
-    //             }
-    //         }
+    //     return Todo.create({
+    //         description: 'Clean yard'
     //     });
-    // }).then(function(todos) {
-    //     if (todos) {
-    //         todos.forEach(function(todo) {
-    //             console.log(todo.toJSON());
-    //         });
-    //     } else {
-    //         console.log('no todo done');
-    //     }
-    // }).catch(function(e) {
-    //     console.log(e);
+    // }).then(function (todo) {
+    //     return User.findById(1).then(function(user) {
+    //         user.addTodo(todo);
+    //     });
     // });
+    
 });
